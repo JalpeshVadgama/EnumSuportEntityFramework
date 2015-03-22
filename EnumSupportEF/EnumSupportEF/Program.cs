296f689c420747e83d18c93cb605f2bff97e6c5c
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-03-22 11:20:41 +0530

    Completed Example

--- EnumSupportEF/EnumSupportEF/Program.cs
+++ EnumSupportEF/EnumSupportEF/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EnumSupportEF
 {
@@ -10,6 +6,30 @@ namespace EnumSupportEF
     {
         static void Main(string[] args)
         {
+            Employee employee = new Employee
+            {
+                EmployeeId = 1,
+                FirstName ="Jalpesh",
+                LastName  ="Vadgama",
+                Deaprtment = Deaprtment.Admin
+            };
+
+            Employee anotherEmployee = new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Vishal",
+                LastName = "Vadgama",
+                Deaprtment = Deaprtment.Marketing
+            };
+
+            //Code for adding Employees
+            using (EmployeeConext employeeConext = new EmployeeConext())
+            {
+                employeeConext.Employees.Add(employee);
+                employeeConext.Employees.Add(anotherEmployee);
+                employeeConext.SaveChanges();
+                Console.WriteLine("Employee Added Sucessfully");
+            }
         }
     }
 }
